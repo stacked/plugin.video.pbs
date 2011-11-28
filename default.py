400b7298977ad19394d1ee89e42308a9899b82dc
stacked <stacked.xbmc@gmail.com>
2011-11-27 22:25:33 -0800

    PBS v1.0.5

--- default.py
+++ default.py
@@ -122,9 +122,8 @@ def build_search_directory( url, page ):
 		listitem = xbmcgui.ListItem( label = '[Next Page (' + str( int( page ) + 2 ) + ')]' , iconImage = next_thumb, thumbnailImage = next_thumb )
 		u = sys.argv[0] + "?mode=6" + "&page=" + str( int( page ) + 1 ) + "&url=" + urllib.quote_plus( save_url )
 		ok = xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = True )
-	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_UNSORTED )
-	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_LABEL )
 	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_STUDIO )
+	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_LABEL )
 	xbmcplugin.endOfDirectory( int( sys.argv[1] ) )
 
 def find_videos( name, program_id, topic, page ):
@@ -134,7 +133,7 @@ def find_videos( name, program_id, topic, page ):
 		data = cove.videos.filter(fields='associated_images,mediafiles,categories',filter_categories__name=name,filter_categories__namespace__name='COVE Taxonomy',order_by='-airdate',filter_type='Episode',filter_producer__name='PBS',filter_availability_status='Available',limit_start=start)['results']
 	elif topic == 'False':
 		data = cove.videos.filter(fields='associated_images,mediafiles',filter_tp_media_object_id=program_id,limit_start=start)['results']
-		print "PBS video ID: " + program_id
+		print "PBS - Video ID: " + program_id
 		if len(data) == 0:
 			dialog = xbmcgui.Dialog()
 			ok = dialog.ok( __plugin__ , __settings__.getLocalizedString( 30009 ) + '\n' + 'http://video.pbs.org/video/' + program_id )
@@ -158,26 +157,33 @@ def find_videos( name, program_id, topic, page ):
 			url = results['mediafiles'][0]['video_download_url']
 			mode = '7'
 		if topic == 'False':
-			play_video( results['title'], url, thumb, results['long_description'], name, None, backup_url )
+			play_video( results['title'], url, thumb, results['long_description'].encode('utf-8'), name, None, backup_url )
 			return
 		if results['mediafiles'][0]['length_mseconds'] != 0:
-			listitem = xbmcgui.ListItem( label = results['title'], iconImage = thumb, thumbnailImage = thumb )
-			listitem.setInfo( type="Video", infoLabels={ "Title": results['title'], "Director": "PBS", "Studio": name, "Plot": results['long_description'].encode('utf-8'), "Duration": str(datetime.timedelta(milliseconds=int(results['mediafiles'][0]['length_mseconds']))), "Aired": results['airdate'].rsplit(' ')[0] } )
-			u = sys.argv[0] + "?mode=" + mode + "&name=" + urllib.quote_plus( results['title'] ) + "&url=" + urllib.quote_plus( url ) + "&thumb=" + urllib.quote_plus( thumb ) + "&plot=" + urllib.quote_plus( results['long_description'].encode('utf-8') ) + "&studio=" + urllib.quote_plus( name ) + "&backup_url=" + urllib.quote_plus( backup_url )
+			listitem = xbmcgui.ListItem( label = results['title'].encode('utf-8'), iconImage = thumb, thumbnailImage = thumb )
+			listitem.setInfo( type="Video", infoLabels={ "Title": results['title'].encode('utf-8'), "Director": "PBS", "Studio": name, "Plot": results['long_description'].encode('utf-8'), "Duration": str(datetime.timedelta(milliseconds=int(results['mediafiles'][0]['length_mseconds']))), "Aired": results['airdate'].rsplit(' ')[0] } )
+			u = sys.argv[0] + "?mode=" + mode + "&name=" + urllib.quote_plus( results['title'].encode('utf-8') ) + "&url=" + urllib.quote_plus( url ) + "&thumb=" + urllib.quote_plus( thumb ) + "&plot=" + urllib.quote_plus( results['long_description'].encode('utf-8') ) + "&studio=" + urllib.quote_plus( name ) + "&backup_url=" + urllib.quote_plus( backup_url )
 			ok = xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = False )
 	if ( len(data) ) == 200:
 		listitem = xbmcgui.ListItem( label = '[Next Page (' + str( int( page ) + 2 ) + ')]' , iconImage = next_thumb, thumbnailImage = next_thumb )
 		u = sys.argv[0] + '?mode=0&name=' + urllib.quote_plus( name ) + '&program_id=' + urllib.quote_plus( program_id ) + "&topic=" + urllib.quote_plus( topic ) + "&page=" + str( int( page ) + 1 )
 		ok = xbmcplugin.addDirectoryItem( handle = int( sys.argv[1] ), url = u, listitem = listitem, isFolder = True )
 	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_UNSORTED )
-	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_VIDEO_RUNTIME )	
+	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_VIDEO_RUNTIME )
+	xbmcplugin.addSortMethod( handle=int( sys.argv[ 1 ] ), sortMethod=xbmcplugin.SORT_METHOD_LABEL )
 	xbmcplugin.endOfDirectory( int( sys.argv[1] ) )
 
 def play_video( name, url, thumb, plot, studio, starttime, backup_url ):
 	data = open_url( url + '&format=SMIL' )
+	print 'PBS - ' + studio + ' - ' + name
+	try:
+		print data
+	except:
+		pass
 	try:
 		base = re.compile( '<meta base="(.+?)" />' ).findall( data )[0]
 	except:
+		print 'PBS - Using backup_url'
 		if backup_url != 'None':
 			play_mp4( name, backup_url, thumb, plot, studio, starttime )
 			return
@@ -189,15 +195,13 @@ def play_video( name, url, thumb, plot, studio, starttime, backup_url ):
 	playpath = None
 	if base == 'http://ad.doubleclick.net/adx/':
 		src_data = src.split( "&lt;break&gt;" )
-		rtmp_url = src_data[0] + "mp4:" + src_data[1]
+		rtmp_url = src_data[0] + "mp4:" + src_data[1].replace('mp4:','')
 	elif base == 'http://www-tc.pbs.org/cove-media/':
-		play_mp4( name, base+src, thumb, plot, studio, starttime )
+		play_mp4( name, base+src.replace('mp4:',''), thumb, plot, studio, starttime )
 		return
-	elif base.find('/vod') != -1:
-		rtmp_url = base
-		playpath = "mp4:" + src
 	else:
-		rtmp_url = base + "mp4:" + src.replace('mp4:','')
+		rtmp_url = base
+		playpath = "mp4:" + src.replace('mp4:','')
 	item = xbmcgui.ListItem( label = name, iconImage = "DefaultVideo.png", thumbnailImage = thumb )
 	item.setInfo( type="Video", infoLabels={ "Title": name , "Director": "PBS", "Studio": "PBS: " + studio, "Plot": plot } )
 	if playpath != None:
